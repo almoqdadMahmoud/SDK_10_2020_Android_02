3f40e8a8e96a0a6f669a66634a2cb69590607cdc
baraa96 <34281301+baraa96@users.noreply.github.com>
2020-10-15 17:47:56 +0300

    Scnner add - Commit

--- Code/JavaReview/src/com/company/Main.java
+++ Code/JavaReview/src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Scanner scan = new Scanner(System.in);
+
+        String id , name ;
+
+        System.out.print("Enter Your Id : ");
+        id = scan.nextLine();
+        System.out.print("Enter Your Name : ");
+        name = scan.nextLine();
+
+        System.out.println("My Id is : " + id);
+        System.out.println("My Name is : " + name);
     }
 }
