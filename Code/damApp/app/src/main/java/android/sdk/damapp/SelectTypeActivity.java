eb8f6d3d395b807ff3317b0dca713e4947e4706d
baraa96 <34281301+baraa96@users.noreply.github.com>
2020-11-09 17:12:40 +0200

    Update

--- Code/damApp/app/src/main/java/android/sdk/damapp/SelectTypeActivity.java
+++ Code/damApp/app/src/main/java/android/sdk/damapp/SelectTypeActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.sdk.damapp.LoginAndRegister.LoginActivity;
+import android.sdk.damapp.LoginAndRegister.PreLoginActivity;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -20,7 +21,7 @@ public class SelectTypeActivity extends AppCompatActivity {
         goToDonor.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(SelectTypeActivity.this, LoginActivity.class);
+                Intent intent = new Intent(SelectTypeActivity.this, PreLoginActivity.class);
                 startActivity(intent);
             }
         });
